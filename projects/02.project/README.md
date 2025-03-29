40956f481892e11011f9f641aaf717a307e5b11e
suriya <suriya@gmail.com>
2025-03-29 23:47:43 +0530

    Projects done

--- projects/02.project/README.md
+++ projects/02.project/README.md
@@ -7,3 +7,5 @@ Step 1 - [CICD with Jenkins](01.md)
 Step 2 - [Static code scan using codeQL](02.md)
 
 Step 3 - [Image scanning using snyk tool](03.md)
+
+Step 4 - [Deployment in AKS cluster](04.md)
---
+++ projects/02.project/tech_asset/azure_aks.tf
@@ -0,0 +1,34 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "aks_rg" {
+  name     = "aks-resource-group"
+  location = "East US"
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "my-aks-cluster"
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  dns_prefix          = "myaksdns"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_D2s_v3"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+output "kube_config" {
+  value     = azurerm_kubernetes_cluster.aks.kube_config_raw
+  sensitive = true
+}
